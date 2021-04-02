9dade7baf21c7ed2abbae9f181e21a08d9845d30
Timothy <timothyroy2011@gmail.com>
2021-04-02 16:23:58 +0600

    filter added

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -16,6 +16,22 @@ class APIFeatures {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    console.log(queryCopy);
+
+    //Removing fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    console.log(queryCopy);
+    //Advance Filter for price, rating etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
