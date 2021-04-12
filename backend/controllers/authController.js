da62b6336cb402454d8a37fb5e7e46521beb2e9d
Timothy <timothyroy2011@gmail.com>
2021-04-12 17:38:17 +0600

    get user by id

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,6 +43,31 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
   sendToken(user, 200, res);
 });
 
+//get currently logged in user details => /api/v1/me
+exports.getUserProfile = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+//Update / user profile => /api/v1/me/update
+exports.updateProfile = catchAsyncError(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  //update avatar : ToDo
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+  });
+});
+
 //Lotgout user => /api/v1/logout
 exports.logout = catchAsyncError(async (req, res, next) => {
   res.cookie("token", null, {
@@ -52,5 +77,31 @@ exports.logout = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "logged out",
+  });
+});
+
+//Admin Routes
+//Get all useres => /api/v1/admin/users
+
+exports.allusers = catchAsyncError(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+//get user details => /api/v1/admin/user/:id
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not found with if: ${req.params.id}`)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    user,
   });
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,25 @@ const {
   registerUser,
   loginUser,
   logout,
+  getUserProfile,
+  updateProfile,
+  allusers,
+  getUserDetails,
 } = require("../controllers/authController");
+
+const { isAuthenticatedUser, athorizeRoles } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/me/update").put(isAuthenticatedUser, getUserDetails);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, athorizeRoles("admin"), allusers);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, athorizeRoles("admin"), getUserDetails);
 
 module.exports = router;
